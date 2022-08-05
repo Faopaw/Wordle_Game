fd9ade2f9306ae4c3659311cbdc81afe17a7cb4f
Faopaw <66549556+Faopaw@users.noreply.github.com>
2022-08-05 22:47:02 +0100

    user inputs unified hint functionality added CSS improved. errors & solved.

--- code.js
+++ code.js
@@ -14,45 +14,46 @@ const WORDLE_DICTIONARY = [
   "IMAGE",
 ];
 
-// Add event listener to form sumbit button and get the userinput and save it to the variable userGuess.
-document.getElementById("word").innerHTML;
-
-
-
-
-// a function to prompt the user to guess the word.
-
-function getWord () {
-    const userGuess = (prompt("Enter your guess in the input box. (5 Letter word, no spaces please.)")).toUpperCase();
-    return userGuess
+// a function to randomly select a word from the WORDLE_DICTIONARY for the current session.
+function selectWord() {
+  const num = Math.floor(Math.random() * WORDLE_DICTIONARY.length);
+  wordleAnswer = WORDLE_DICTIONARY[num];
+  return wordleAnswer;
 }
 
-// a function to randomly select a word from the WORDLE_DICTIONARY for the current session.     -COMPLETED-
+let wordleAnswer = "";
+let hintCount = 0;
+window.onload = selectWord;
 
-function selectWord () {
-    const num =  Math.floor(Math.random() * WORDLE_DICTIONARY.length)
-    const word = WORDLE_DICTIONARY[num];
-    return word;
+function hintGiver(ans) {
+  // gives the first and last character of the correct answer.
+  let firstChar = ans.substring(0, 1);
+  let lastChar = ans.slice(-1);
+  alert(
+    `Hint: the first character is ${firstChar} and the last character is ${lastChar}.`
+  );
 }
 
+function getWord() {
+  // const userGuess = (prompt("Enter your guess in the input box. (5 Letter word, no spaces please.)")).toUpperCase();
+  const userGuess = alert("Please enter a guess in the input box");
+  return userGuess;
+}
 
 // Main function to run the game;
-function runGame () {
-    let userGuess = (document.getElementById("inputbox").value).toUpperCase();
-    let word = selectWord();
-    // let userGuess = (prompt("Enter your guess in the input box. (5 Letter word, no spaces please.)")).toUpperCase();
-    while (!(userGuess === word)) {
-        userGuess = getWord();
-        if (userGuess === word) {
-            continue;
-        }
-        alert("Please try again!")
+function runGame() {
+  let userGuess = document.getElementById("inputbox").value.toUpperCase();
+  let word = wordleAnswer;
+
+  if (!(userGuess === word)) {
+    hintCount + 1;
+    alert("Please try again!");
+    if (hintCount % 5 === 0) {
+      // A hint is given every 5 attempts
+      hintGiver(word);
     }
+  }
+  if (userGuess === word) {
     alert("Correct! You have got the right answer!");
+  }
 }
-
-
-runGame();
-
-
-
